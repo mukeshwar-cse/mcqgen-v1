090d7a8cc4a349610ee927d0403fffe1c65ecc25
mukeshwar-cse <mukeshwarnath.cse@gmail.com>
2024-12-09 21:54:39 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Mukeshwar Nath',
+    author_email='mukeshwarnath.cse@ineuron.ai',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
